f65118dd13bba643de9f8c0c87881e7694db0e75
Jese <jeseravi@gmail.com>
2022-06-30 08:08:03 -0400

    Tweak layout

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import{generateElement} from "./generateElement.js";
 
-generateElement("div","header","","content","header","")
+generateElement("div","header","Header","content","header","")
 generateElement("div","centerContainer","","content","centerContainer","")
 generateElement("div","sidebarContainer","","centerContainer","sidebarContainer","")
-generateElement("div","sidebar","","sidebarContainer","sidebar","")
-generateElement("div","mainContentContainer","","centerContainer","mainContentContainer","")
+generateElement("div","sidebar","Sidebar","sidebarContainer","sidebar","")
+generateElement("div","mainContentContainer","main content","centerContainer","mainContentContainer","")
 generateElement("div","mainContent","","mainContentContainer","mainContent","")
-generateElement("div","footer","","content","footer","")
+generateElement("div","footer","footer","content","footer","")
 
 
 
